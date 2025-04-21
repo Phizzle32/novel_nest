4b752933686b3754b52db15633d0e1b02d184b17
Phi Le <phizzle2003@gmail.com>
2025-04-21 13:14:21 -0400

    Add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:novel_nest/firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(NovelNest());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class NovelNest extends StatelessWidget {
+  const NovelNest({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Novel Nest',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
