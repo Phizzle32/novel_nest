fd61ad182554e65c019f2051b04ae9d3372d87e5
Phi Le <phizzle2003@gmail.com>
2025-05-03 01:52:56 -0400

    Implement book details screen - Displays book details - Shows user's reading status
    TODO: implement reviews

--- lib/screens/book_details_screen.dart
+++ lib/screens/book_details_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 import 'package:novel_nest/models/app_user.dart';
 import 'package:novel_nest/models/book.dart';
 import 'package:novel_nest/models/reading_list_entry.dart';
@@ -20,10 +21,19 @@ class BookDetailsScreen extends StatefulWidget {
 }
 
 class _BookDetailsScreenState extends State<BookDetailsScreen> {
-  AppUser? currentUser;
   late final Book book;
-  late final ReadingListEntry? readingListEntry;
+  AppUser? currentUser;
+  ReadingListEntry? readingListEntry;
+  String? readingStatus;
   bool isLoading = true;
+
+  final List<String> statuses = [
+    'Plan to Read',
+    'Currently Reading',
+    'Finished',
+    'Dropped',
+    'Remove from Reading List',
+  ];
 
   @override
   void initState() {
@@ -38,7 +48,7 @@ class _BookDetailsScreenState extends State<BookDetailsScreen> {
 
     final user = await authService.getCurrentUser();
     final bookDetails = await bookService.getBookById(widget.bookId);
-    final readingListEntryDetails = await firestoreService.getReadingListEntry(
+    readingListEntry = await firestoreService.getReadingListEntry(
       userId: user?.id ?? '',
       bookId: widget.bookId,
     );
@@ -46,9 +56,87 @@ class _BookDetailsScreenState extends State<BookDetailsScreen> {
     setState(() {
       currentUser = user;
       book = bookDetails;
-      readingListEntry = readingListEntryDetails;
+      readingStatus = readingListEntry?.status;
       isLoading = false;
     });
+  }
+
+  Widget _buildBookImage() {
+    if (book.thumbnail != null) {
+      return Padding(
+        padding: const EdgeInsets.all(15),
+        child: Image.network(
+          book.thumbnail!,
+          width: 120,
+          height: 180,
+          fit: BoxFit.cover,
+        ),
+      );
+    } else {
+      return const Icon(
+        Icons.book,
+        size: 120,
+        color: Colors.grey,
+      );
+    }
+  }
+
+  String _getBookRating() {
+    if (book.averageRating == null) {
+      return "N/A";
+    }
+    return '${book.averageRating!.toStringAsFixed(1)} / 5';
+  }
+
+  Future<void> _updateReadingListEntry(String? newStatus) async {
+    if (newStatus == null || currentUser == null) {
+      return;
+    }
+
+    // Check for meaningless updates
+    if (readingStatus == newStatus ||
+        (readingListEntry == null && newStatus == 'Remove from Reading List')) {
+      return;
+    }
+
+    setState(() {
+      readingStatus =
+          (newStatus == 'Remove from Reading List') ? null : newStatus;
+    });
+
+    final firestoreService = context.read<FirestoreService>();
+
+    try {
+      if (newStatus == 'Remove from Reading List') {
+        await firestoreService.deleteReadingListEntry(
+          userId: currentUser!.id,
+          bookId: widget.bookId,
+        );
+        readingListEntry = null;
+      } else if (readingListEntry == null) {
+        await firestoreService.addToReadingList(
+          book: book,
+          userId: currentUser!.id,
+          status: newStatus,
+        );
+        readingListEntry = await firestoreService.getReadingListEntry(
+          userId: currentUser!.id,
+          bookId: widget.bookId,
+        );
+      } else {
+        await firestoreService.updateReadingListEntry(
+          userId: currentUser!.id,
+          bookId: widget.bookId,
+          status: newStatus,
+        );
+      }
+    } catch (e) {
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Failed to update reading list')),
+        );
+      }
+    }
   }
 
   @override
@@ -57,14 +145,102 @@ class _BookDetailsScreenState extends State<BookDetailsScreen> {
       appBar: const NovelNestAppBar(showBackButton: true),
       drawer: const NovelNestDrawer(),
       body: AppBackground(
-        child: Center(
-          child: isLoading
-              ? const CircularProgressIndicator()
-              : Text(
-                  book.title,
-                  style: Theme.of(context).textTheme.headlineLarge,
+        child: isLoading
+            ? const Center(child: CircularProgressIndicator())
+            : SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 15,
+                        horizontal: 8,
+                      ),
+                      child: Text(
+                        book.title,
+                        style: Theme.of(context).textTheme.headlineLarge,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 15,
+                      ),
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        border: Border.all(color: Colors.blueGrey),
+                        color: const Color(0xFFF5F5F5),
+                        boxShadow: [
+                          const BoxShadow(
+                            color: Colors.black26,
+                            blurRadius: 8,
+                            offset: Offset(0, 4),
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        children: [
+                          _buildBookImage(),
+                          Text(
+                            book.title,
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                          Text(
+                            book.authors?.join(', ') ?? 'Unknown Author',
+                            textAlign: TextAlign.center,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            spacing: 5,
+                            children: [
+                              const Icon(Icons.star, color: Colors.amber),
+                              Text(_getBookRating()),
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              spacing: 5,
+                              children: [
+                                const Text(
+                                  'Reading Status:',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                DropdownButton<String>(
+                                  value: readingStatus,
+                                  hint: const Text('Select status'),
+                                  borderRadius: BorderRadius.circular(5),
+                                  items: statuses.map((String status) {
+                                    return DropdownMenuItem<String>(
+                                      value: status,
+                                      child: Text(status),
+                                    );
+                                  }).toList(),
+                                  onChanged: _updateReadingListEntry,
+                                ),
+                              ],
+                            ),
+                          ),
+                          Html(
+                            data: book.description?.replaceAll(
+                                    RegExp(r'(<br\s*/?>)+$'), '') ??
+                                'No description available',
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
                 ),
-        ),
+              ),
       ),
     );
   }
