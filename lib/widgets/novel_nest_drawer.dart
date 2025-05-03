a10d7f54f4cc3de37664d99a3003195484c7cbc6
BeestingGit <anthonypaulchong@gmail.com>
2025-05-03 13:21:29 -0400

    Implemented Reading List screen

--- lib/widgets/novel_nest_drawer.dart
+++ lib/widgets/novel_nest_drawer.dart
@@ -3,6 +3,7 @@ import 'package:novel_nest/models/app_user.dart';
 import 'package:novel_nest/screens/book_search_screen.dart';
 import 'package:novel_nest/screens/discussion_board_screen.dart';
 import 'package:novel_nest/screens/profile_screen.dart';
+import 'package:novel_nest/screens/reading_list_screen.dart';
 import 'package:novel_nest/services/auth_service.dart';
 import 'package:provider/provider.dart';
 
@@ -112,8 +113,8 @@ class _NovelNestDrawerState extends State<NovelNestDrawer> {
               onTap: () {
                 Navigator.pushReplacement(
                   context,
-                  MaterialPageRoute(builder: (context) => BookSearchScreen()),
-                ); // Change to Reading list screen
+                  MaterialPageRoute(builder: (context) => ReadingListScreen()),
+                );
               },
             ),
             ListTile(
